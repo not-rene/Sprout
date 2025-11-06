f225a5bc78a0ca8ac652c4b2ada4a3737503c87b
Allie Elliott <elliottallie28@gmail.com>
2025-11-06 13:08:05 -0600

    fixed some errors that I left last time. I believe one was unused export, i forgot

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SproutApp());
+  runApp(const SproutApp());
 }
 
 class SproutApp extends StatelessWidget {
+  // changed: use super parameter
+  const SproutApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sprout App',
+      title: 'Sprout',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
+  // changed: use super parameter
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome to Sprout 🌱'),
+        title: const Text('Sprout 🌱'),
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          'Hello, world!',
+          'Welcome to Sprout!',
           style: TextStyle(fontSize: 24),
         ),
       ),
